6dfc9fee820f3205c7a378afb1d9e271abd4add9
samedfft2634 <samedfft@gmail.com>
2023-10-10 17:47:36 +0300

    third commit

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 let countEl = document.getElementById("count-el")
-
+let saveEl = document.getElementById("save-el")
+let currentEl = document.getElementById("current-temp")
 let count = 0
 
 function increment() {
     count += 1
     countEl.innerText = count
+}
+
+function decrement() {
+    count -= 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " "
+    currentEl.textContent += countStr
+    countEl.textContent = 0
+    currentEl.innerText = ""
+    currentEl.innerText = count
+    count = 0
 }
