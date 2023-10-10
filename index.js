d45a528917a6a8f73b3fd42c3d67a898d04479aa
samedfft2634 <samedfft@gmail.com>
2023-10-10 17:04:59 +0300

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.getElementById("count-el").innerText = 5;
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
